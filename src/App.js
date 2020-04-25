00b22aa5145b505eb0dc681c7ee829c50fde930e
Muhotasim Fuad <codemen.fuad@gmail.com>
2020-04-25 22:05:26 +0600

    updated react starter app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { title: "" };
+    this.state = {};
     [].forEach((fn) => (this[fn] = this[fn].bind(this)));
   }
 
